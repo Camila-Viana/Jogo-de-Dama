486b1e2651a8180b14674c0a364e3a7d973df709
Daniel Lopes <danielpolido2015@gmail.com>
2019-11-04 22:31:21 -0300

    Update Code

--- Projeto-Jogo-de-Damas/Damas.h
+++ Projeto-Jogo-de-Damas/Damas.h
@@ -1,8 +1,54 @@
 #ifndef DAMAS_H_
 #define DAMAS_H_
 
-void inicializador();
+void Inicializador();
 
+class Peça {
+public:
+	virtual void movimento();
+	virtual void opçõesMovimento();
+};
 
+class PeçaNormal : public Peça {
+private:
+	Jogador _jogador;
+	int _PosX;
+	int _PosY;
+public:
+	PeçaNorma();
+	~PeçaNormal();
+	void movimento(int _PosX, int _PosY) override;
+	void opçõesMovimento(int _PosX, int _PosY) override;
+	void getPositionX(int _PosX);
+	void getPositionY(int _PosY);
+};
+
+class PeçaDama : public Peça {
+private:
+	Jogador jogador;
+	int PosX;
+	int PosY;
+public:
+	PeçaDama(Jogador _jogador, int _PosX, int _PosY);
+	~PeçaDama();
+	void movimento(int PosX, int PosY) override;
+	void opçõesmovimento(int PosX, int PosY) override;
+	void getPositionX(int PosX);
+	void getPositionY(int PosY);
+};
+
+class Jogador {
+private:
+	char Nome;
+	int Npeças;
+public:
+	Jogador();
+	~Jogador();
+	char getname(char Nome);
+	void decreaseNpeças(int Npeças);
+	int getNpeças(Npeças);
+};
+
+void DestrutorFinal();
 
 #endif 
